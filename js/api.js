1a4b58bd7be77fb626e4a862ff5af03cabbae349
Md Hazrat Ali <hazratali4936@gmail.com>
2023-08-03 12:21:20 +0200

    start the project and finish the implementation of home page

---
+++ js/api.js
@@ -0,0 +1,61 @@
+// const searchField = document.getElementById("search-input");
+
+// searchField.addEventListener(
+//   "input",
+//   (handleSearch = () => {
+//     const searchText = searchField.value.trim();
+
+//     // If the search text is empty, display all data
+//     if (searchText === "") {
+//       profilesData();
+//     } else {
+//       //get the profiles data
+//       const url = `https://2023.projektbigfoot.de/api/v1/projects?query=${encodeURIComponent(
+//         searchText
+//       )}`;
+//       fetch(url)
+//         .then((res) => res.json())
+//         .then((data) => {
+//           const filteredData = data.filter((item) =>
+//             item.contestantName.toLowerCase().includes(searchText.toLowerCase())
+//           );
+//           displayProfiles(filteredData)
+//         });
+//     }
+//   })
+// );
+// //clear the searchfield
+// searchField.value = "";
+
+const profilesData = () => {
+  const url = `https://2023.projektbigfoot.de/api/v1/projects`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayProfiles(data));
+};
+
+profilesData();
+
+
+const displayProfiles = (profiles) => {
+  const tableContainer = document.getElementById("table-container");
+
+  profiles.map((profile) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <table>
+      <tbody>
+        <tr>
+          <td>${profile?.rank + "."}</td>
+            <td>${profile?.voteCount}</td>
+            <td>
+              <h4>${profile?.contestantName}</h4>
+              <p>${profile?.projectTitle}</p>
+            </td>      
+        </tr>
+       <tbody>     
+    </table>     
+    `;
+    tableContainer.appendChild(div);
+  });
+};
